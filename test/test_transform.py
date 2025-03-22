b1eec139bd4e7d7a2c0aea7699921bbe53608e56
Temitope Bamidele <57126906+bamidele42@users.noreply.github.com>
2025-03-22 08:06:08 +0100

    Delete test directory

--- test/test_transform.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from web_scraper import movies, web_scrapper
-from data_transform import result, clean_data
-import pandas as pd
-
-def test_transfrom():
-    output = result(movies)
-    assert output == pandas.core.frame.DataFrame
